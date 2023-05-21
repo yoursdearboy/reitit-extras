eb5f248beebcdacbe324ee30fe4c06fabbe1cb86
Kirill Voronin <yoursdearboy@gmail.com>
2023-05-22 01:07:32 +0300

    Moved servlet support to yoursdearboy/reitit-servlet

--- src/reitit/extras/routes.clj
+++ src/reitit/extras/routes.clj
@@ -1,22 +1,22 @@
 (ns reitit.extras.routes
-  "Reverse routing (to URI string) for reitit with servlet's context support."
+  "Reverse routing (to URI string)."
   (:require [clojure.string :as str]
-            [reitit.core :as r]))
+            [reitit.core :as r]
+            [reitit.impl :as impl]))
 
 (defn path-for*
   ([request name] (path-for* request name {}))
   ([request name query-params] (path-for* request name query-params {}))
-  ([request name query-params path-params]
-   (str
-    (:servlet-context-path request "") "/"
-    (str/replace-first
-     (cond (string? name) name
-           :else (r/match->path
-                  (r/match-by-name
-                   (request :reitit.core/router)
-                   name
-                   query-params) path-params))
-     #"^/" ""))))
+  ([request name query-params path-params] (path-for* request name query-params path-params :encode true))
+  ([request name query-params path-params & {:keys [encode]}]
+   (let [path (cond (string? name) name
+                    :else (r/match->path
+                           (r/match-by-name
+                            (request :reitit.core/router)
+                            name
+                            query-params)
+                           path-params))]
+     (if encode path (impl/url-decode path)))))
 
 (defmacro path-for [& args]
   `(path-for* ~'request ~@args))
